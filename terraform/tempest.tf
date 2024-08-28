91dcabcd4de19462b41b1d3a1cefce9c7b3016ff
bruvio <bruno.viola@genomicsengland.co.uk>
2024-08-28 07:37:33 +0200

    fix: refactor

---
+++ terraform/tempest.tf
@@ -0,0 +1,21 @@
+module "bruvio" {
+  source = "git::https://github.com/poseidon/typhoon//aws/flatcar-linux/kubernetes?ref=v1.30.3"
+
+  # AWS
+  cluster_name = "bruvio"
+  dns_zone     = "brunoviola.net."
+  dns_zone_id  = "Z01104713294GHMBIFP6F"
+
+  # instances
+  worker_count = 2
+  worker_type  = "t3.small"
+
+  # configuration
+  ssh_authorized_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC12i3jUNdHxEWhbngziqUjgXw86Zrnv3vwjQfmovidRbFMB91LSnkBfEqI9OBxWCIWc80VlO9UD6UlPnR9h4LTKXdXUc/m84OEHIe24LdZB7sWei4iOjlC/FQkccH/xRhshBG0IcC3SC1Gvq7bLgoXO5BlLhQS5z1N/H5jaZPxytezw5bmOwdt5ST1ZkQMxn1nVkAY94fQAzbx79/nsHciVzURwhjlzq4GII5E1ojm0Up+m7HKshpcmPaBCz2WS3M1KDGl+BYJ0E7zQavJm/ImwE1pkhraIvunmP4kFIbluW03rY83K7LZr99Q3ZWo4UNwLKV4YFy+YyLJCYmlZokb brunoviola@192.168.1.53"
+}
+
+resource "local_file" "kubeconfig-bruvio" {
+  content  = module.bruvio.kubeconfig-admin
+  filename = "bruvio-config"
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
+}
+variable "key-name" {
+  default = "deployer-key"
+}
+
+
