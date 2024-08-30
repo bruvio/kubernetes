62f73f1f832a5bb7368953904a33568d7785b042
bruvio <bruno.viola@genomicsengland.co.uk>
2024-08-30 07:57:56 +0200

    fix final nginx

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -26,16 +26,7 @@ resource "local_file" "kubeconfig-bruvio" {
   filename = "bruvio-config"
 }
 
-resource "aws_route53_record" "some-application" {
-  # DNS zone ID
-  zone_id = "Z01104713294GHMBIFP6F" # Replace with your Route 53 Hosted Zone ID
 
-  # DNS record
-  name    = "app.brunoviola.net."
-  type    = "CNAME"
-  ttl     = 300
-  records = ["${module.bruvio.ingress_dns_name}."]
-}
 
 locals {
   custom_components = {
