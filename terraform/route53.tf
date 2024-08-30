0688845989ef490967e093d2f6c415a0429ac300
bruvio <bruno.viola@genomicsengland.co.uk>
2024-08-30 07:56:23 +0200

    fix final nginx

---
+++ terraform/route53.tf
@@ -0,0 +1,10 @@
+resource "aws_route53_record" "some-application" {
+  # DNS zone ID
+  zone_id = "Z01104713294GHMBIFP6F" # Replace with your Route 53 Hosted Zone ID
+
+  # DNS record
+  name    = "app.brunoviola.net."
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${module.bruvio.ingress_dns_name}."]
+}
