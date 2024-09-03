3a53ac90c340ff979a2a634cec18f9d2db047057
bruvio <bruno.viola@genomicsengland.co.uk>
2024-09-03 11:22:28 +0100

    fix: add local and public ingress test

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_record" "some-application" {
   # DNS zone ID
-  zone_id = "Z01104713294GHMBIFP6F" # Replace with your Route 53 Hosted Zone ID
+  zone_id = "Z01104713294GHMBIFP6F" 
 
   # DNS record
-  name    = "app.brunoviola.net."
+  name    = "app.bruvio.brunoviola.net."
   type    = "CNAME"
   ttl     = 300
   records = ["${module.bruvio.ingress_dns_name}."]
