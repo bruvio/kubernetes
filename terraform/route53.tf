2e7f4ba80e04c46265227341641bddd30f055564
bruvio <bruno.viola@genomicsengland.co.uk>
2024-09-03 15:20:21 +0100

    chore: remove unused files

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "some-application" {
   # DNS zone ID
-  zone_id = "Z01104713294GHMBIFP6F" 
+  zone_id = "Z01104713294GHMBIFP6F"
 
   # DNS record
   name    = "app.bruvio.brunoviola.net."
