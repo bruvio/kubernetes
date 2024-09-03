cc3ef68ff3ceff76f0202b75a48b9b90c41697b0
bruvio <bruno.viola@genomicsengland.co.uk>
2024-09-03 12:20:56 +0100

    fix: getting healthy target using Typhoon nginx addon controller

--- terraform/check_k8s_nodes.sh
+++ terraform/check_k8s_nodes.sh
@@ -5,7 +5,13 @@ export KUBECONFIG=bruvio-config
 # Function to check the status of all nodes
 check_nodes_status() {
   # Get the status of all nodes
-  node_status=$(kubectl get nodes | grep -v NAME | awk '{print $2}')
+  node_status=$(kubectl get nodes 2>/dev/null | grep -v NAME | awk '{print $2}')
+
+  # Check if no nodes are found
+  if [ -z "$node_status" ]; then
+    echo "No nodes found"
+    return 1
+  fi
 
   # Check if all nodes are in the "Ready" state
   all_ready=true
@@ -16,18 +22,20 @@ check_nodes_status() {
     fi
   done
 
-  echo $all_ready
+  if [ "$all_ready" = true ]; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 # Loop until all nodes are ready
 while true; do
-  all_ready=$(check_nodes_status)
-
-  if [ "$all_ready" = true ]; then
+  if check_nodes_status; then
     echo "All Kubernetes nodes are ready!"
     break
   else
-    echo "Not all nodes are ready yet. Checking again in 10 seconds..."
+    echo "Not all nodes are ready yet or no nodes found. Checking again in 10 seconds..."
     sleep 10
   fi
 done
